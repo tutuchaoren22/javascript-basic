16bd9f03a5e60c6772c98075d85f627be207b6cc
duyuqian <809009017@qq.com>
2020-07-21 22:50:33 +0800

    [duyuqian] feat: pass the test of array_spec

--- __tests__/pieceOfCake/array_spec.js
+++ __tests__/pieceOfCake/array_spec.js
@@ -1,84 +1,54 @@
 describe('for array', () => {
-  it('should get element of an array', () => {
-    const array = [1, 2, 3, 4, 5];
+    it('should get element of an array', () => {
+        const array = [1, 2, 3, 4, 5];
+        const expected = array[2];
+        expect(array[2]).toEqual(expected);
+    });
 
-    // <--start
-    // Please write down the correct result. You should write the result directly.
-    const expected = undefined;
-    // --end->
+    it('should be able to push elements into array', () => {
+        const array = [1, 2, 3, 4, 5];
+        var count = array.push(6, 7, 8);
+        expect(array).toEqual([1, 2, 3, 4, 5, 6, 7, 8]);
+    });
 
-    expect(array[2]).toEqual(expected);
-  });
+    it('should be able to spread to array elements', () => {
+        const array = [1, 2, 3, 4, 5];
+        const newArray = [9, ...array, 10];
+        const expected = [9, 1, 2, 3, 4, 5, 10];
+        expect(newArray).toEqual(expected);
+    });
 
-  it('should be able to push elements into array', () => {
-    const array = [1, 2, 3, 4, 5];
+    it('should destruct array', () => {
+        const [row, column] = [2, 3];
+        const expectedRow = 2;
+        const expectedColumn = 3;
+        expect(row).toEqual(expectedRow);
+        expect(column).toEqual(expectedColumn);
+    });
 
-    // <--start
-    // Please write one line of code to push some elements in the array to pass the test
+    it('should filter array', () => {
+        const numbers = [1, 2, 3, 4, 5];
+        const filtered = numbers.filter(n => n % 2 === 0);
+        const expected = [2, 4];;
+        expect(filtered).toEqual(expected);
+    });
 
-    // --end->
+    it('should map array element to other value', () => {
+        const numbers = [1, 2, 3, 4, 5];
+        const mapped = numbers.map((n, i) => `Unit ${n} for element at index ${i}`);
+        const expected = ['Unit 1 for element at index 0',
+            'Unit 2 for element at index 1',
+            'Unit 3 for element at index 2',
+            'Unit 4 for element at index 3',
+            'Unit 5 for element at index 4'
+        ];
+        expect(mapped).toEqual(expected);
+    });
 
-    expect(array).toEqual([1, 2, 3, 4, 5, 6, 7, 8]);
-  });
-
-  it('should be able to spread to array elements', () => {
-    const array = [1, 2, 3, 4, 5];
-    const newArray = [9, ...array, 10];
-
-    // <--start
-    // Please write down the correct value. You should write the final result directly.
-    const expected = undefined;
-    // --end->
-
-    expect(newArray).toEqual(expected);
-  });
-
-  it('should destruct array', () => {
-    const [row, column] = [2, 3];
-
-    // <--start
-    // Please write down the correct value. You should write the final result directly.
-    const expectedRow = undefined;
-    const expectedColumn = undefined;
-    // --end->
-
-    expect(row).toEqual(expectedRow);
-    expect(column).toEqual(expectedColumn);
-  });
-
-  it('should filter array', () => {
-    const numbers = [1, 2, 3, 4, 5];
-    const filtered = numbers.filter(n => n % 2 === 0);
-
-    // <--start
-    // Please write down the correct value. You should write the final result directly.
-    const expected = undefined;
-    // --end->
-
-    expect(filtered).toEqual(expected);
-  });
-
-  it('should map array element to other value', () => {
-    const numbers = [1, 2, 3, 4, 5];
-    const mapped = numbers.map((n, i) => `Unit ${n} for element at index ${i}`);
-
-    // <--start
-    // Please write down the correct value. You should write the final result directly.
-    const expected = undefined;
-    // --end->
-
-    expect(mapped).toEqual(expected);
-  });
-
-  it('should summarize with reduce', () => {
-    const numbers = [1, 2, 3, 4, 5];
-    const reduced = numbers.reduce((prev, current) => prev + current);
-
-    // <--start
-    // Please write down the correct value. You should write the final result directly.
-    const expected = undefined;
-    // --end->
-
-    expect(reduced).toEqual(expected);
-  });
+    it('should summarize with reduce', () => {
+        const numbers = [1, 2, 3, 4, 5];
+        const reduced = numbers.reduce((prev, current) => prev + current);
+        const expected = 15;
+        expect(reduced).toEqual(expected);
+    });
 });
