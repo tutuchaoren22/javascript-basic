e1f917da37f8fd0729d091af7d4303c2a2cc0ae7
duyuqian <809009017@qq.com>
2020-07-22 11:35:02 +0800

    [duyuqian] feat: pass the test of variable_spec

--- __tests__/pieceOfCake/variable_spec.js
+++ __tests__/pieceOfCake/variable_spec.js
@@ -1,41 +1,21 @@
 describe('for variable', () => {
-  it('should have function scope for var variable', () => {
-    // eslint-disable-next-line vars-on-top, no-var, no-empty
-    for (var i = 0; i <= 5; i += 1) { }
+    it('should have function scope for var variable', () => {
+        for (var i = 0; i <= 5; i += 1) {}
+        const expected = 6;
+        expect(i).toEqual(expected);
+    });
 
-    // <--start
-    // Please write down the correct value. You should write the final result directly.
-    const expected = undefined;
-    // --end->
+    it('should have block scope for let and const variable', () => {
+        let i = 1000;
+        for (let i = 0; i <= 5; i += 1) {}
+        const expected = 1000;
+        expect(i).toEqual(expected);
+    });
 
-    // eslint-disable-next-line block-scoped-var
-    expect(i).toEqual(expected);
-  });
-
-  it('should have block scope for let and const variable', () => {
-    // eslint-disable-next-line prefer-const
-    let i = 1000;
-    // eslint-disable-next-line no-empty, no-shadow
-    for (let i = 0; i <= 5; i += 1) { }
-
-    // <--start
-    // Please write down the correct value. You should write the final result directly.
-    const expected = undefined;
-    // --end->
-
-    // eslint-disable-next-line no-undef
-    expect(i).toEqual(expected);
-  });
-
-  it('should be able to change details of const variable', () => {
-    const constVariable = { name: 'changeit' };
-    constVariable.name = 'new name';
-
-    // <--start
-    // Please write down the correct value. You should write the final result directly.
-    const expected = undefined;
-    // --end->
-
-    expect(constVariable.name).toEqual(expected);
-  });
+    it('should be able to change details of const variable', () => {
+        const constVariable = { name: 'changeit' };
+        constVariable.name = 'new name';
+        const expected = 'new name';
+        expect(constVariable.name).toEqual(expected);
+    });
 });
