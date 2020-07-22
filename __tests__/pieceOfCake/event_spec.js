c74cca6d9b6c7e248a23431aa8910b54d6273540
duyuqian <809009017@qq.com>
2020-07-22 20:06:43 +0800

    [duyuqian] feat: pass the test of event_spec

--- __tests__/pieceOfCake/event_spec.js
+++ __tests__/pieceOfCake/event_spec.js
@@ -1,67 +1,57 @@
 import EventEmitter from 'events';
 
 describe('for event', () => {
-  function waitForEvents() {
-    return new Promise(resolve => setTimeout(() => resolve(), 0));
-  }
-
-  it('should capture event', (done) => {
-    const element = new EventEmitter();
-    const logs = [];
-
-    function onClick() {
-      logs.push('I have been clicked.');
-      done();
+    function waitForEvents() {
+        return new Promise(resolve => setTimeout(() => resolve(), 0));
     }
 
-    // <--start
-    // Please add the event listener to handle `click` event on `element`.
+    it('should capture event', (done) => {
+        const element = new EventEmitter();
+        const logs = [];
 
-    // --end->
+        function onClick() {
+            logs.push('I have been clicked.');
+            done();
+        }
 
-    element.emit('click');
-  }, 1000 /* 1 second to timeout */);
+        element.addListener('click', () => {
+            onClick();
+        });
+        element.emit('click');
+    }, 1000 /* 1 second to timeout */ );
 
-  it('should invoke multiple times', (done) => {
-    const element = new EventEmitter();
-    const logs = [];
+    it('should invoke multiple times', (done) => {
+        const element = new EventEmitter();
+        const logs = [];
 
-    element.addListener('click', () => logs.push('I have been clicked'));
-    element.emit('click');
-    element.emit('click');
+        element.addListener('click', () => logs.push('I have been clicked'));
+        element.emit('click');
+        element.emit('click');
 
-    waitForEvents()
-      .then(() => {
-        // <--start
-        // Please write down the correct value. You should write the final result directly.
-        const expected = undefined;
-        // --end->
-
-        expect(logs).toEqual(expected);
-        done();
-      });
-  });
-
-  it('should remove event listener', (done) => {
-    const element = new EventEmitter();
-    const logs = [];
-
-    element.addListener('click', () => {
-      logs.push('I have been clicked');
-      element.removeAllListeners('click');
+        waitForEvents()
+            .then(() => {
+                const expected = ['I have been clicked', 'I have been clicked'];
+                expect(logs).toEqual(expected);
+                done();
+            });
     });
-    element.emit('click');
-    element.emit('click');
 
-    waitForEvents()
-      .then(() => {
-        // <--start
-        // Please write down the correct value. You should write the final result directly.
-        const expected = undefined;
-        // --end->
+    it('should remove event listener', (done) => {
+        const element = new EventEmitter();
+        const logs = [];
 
-        expect(logs).toEqual(expected);
-        done();
-      });
-  });
+        element.addListener('click', () => {
+            logs.push('I have been clicked');
+            element.removeAllListeners('click');
+        });
+        element.emit('click');
+        element.emit('click');
+
+        waitForEvents()
+            .then(() => {
+                const expected = ['I have been clicked'];
+                expect(logs).toEqual(expected);
+                done();
+            });
+    });
 });
